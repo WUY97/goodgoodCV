aa8937de81e456392bfc23bf440c5c87291a6a00
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2022-09-28 22:11:38 -0700

    modify server.js

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
-
-const port = process.env.PORT || 5000;
-const path = require('path');
-
 const dbConnect = require('./dbConnect');
+app.use(express.json());
+const port = process.env.PORT || 5000;
 const userRoute = require('./routes/userRoute');
+const path = require('path');
+app.use('/api/user/', userRoute);
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static('client/build'));
+
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
-app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-app.use('/api/user/', userRoute);
